8ba582c789399f0e82795252bdb5c987ff40cd31
SnS <SnS@SLEVIN-PC>
2023-05-16 19:43:41 +0530

    initicvdsfAdd project files.

---
+++ ProductsAPI/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using ProductsAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace ProductsAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if (user.Username == "admin" && user.Password == "12345")
+            {
+                _user = new Users { Username = "Shiny" };
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users user)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null, expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var _user = AuthenticateUser(user);
+            if (_user != null)
+            {
+                var token = GenerateToken(_user);
+                response = Ok(new { token = token });
+            }
+            return response;
+
+        }
+    }
+}
